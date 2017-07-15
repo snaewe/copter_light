2ae12abd101837cddbc5e762102268bd12f1f396
Stefan Naewe <stefan.naewe@gmail.com>
2017-07-15 13:54:23 +0200

    add fade_all on startup,
    Signed-off-by: Stefan Naewe <stefan.naewe@gmail.com>

--- copter_light.ino
+++ copter_light.ino
@@ -15,6 +15,12 @@ void setup()
 
 void test_all()
 {
+  blink_all();
+  fade_all();
+}
+
+void blink_all()
+{
   blink(NW_pin, 500, 200);
   delay(500);
   blink(NE_pin, 500, 200);
@@ -22,6 +28,17 @@ void test_all()
   blink(SE_pin, 500, 200);
   delay(500);
   blink(SW_pin, 500, 200);
+}
+
+void fade_all()
+{
+  fade(NW_pin, 0, 255, 50);
+  delay(500);
+  fade(SW_pin, 0, 255, 50);
+  delay(500);
+  fade(SE_pin, 0, 255, 50);
+  delay(500);
+  fade(NE_pin, 0, 255, 50);
 }
 
 void blink(byte pin, unsigned long lightMillis, unsigned long pauseMillis) {
@@ -34,6 +51,14 @@ void blink(byte pin, unsigned long lightMillis, unsigned long pauseMillis) {
   digitalWrite(pin, 0);
 }
 
+void fade(byte pin, byte from, byte to, unsigned long pauseMillis)
+{
+  for(byte val=from; val<to; val+=5)
+  {
+    analogWrite(pin, val);
+    delay(pauseMillis);  
+  }
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
