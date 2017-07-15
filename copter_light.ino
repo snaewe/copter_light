fa3e7aeb76977e6096b1b71b497e43b9420a085c
Stefan Naewe <stefan.naewe@gmail.com>
2017-07-15 14:11:30 +0200

    split fade() into fadeUp() and fadeDown()
    Signed-off-by: Stefan Naewe <stefan.naewe@gmail.com>

--- copter_light.ino
+++ copter_light.ino
@@ -40,13 +40,15 @@ void blink_all()
 
 void fade_all()
 {
-  fade(NW_pin, 0, 255, 50);
+  fadeUp(NW_pin, 0, 255, 50);
+  fadeUp(SW_pin, 0, 255, 50);
+  fadeUp(SE_pin, 0, 255, 50);
+  fadeUp(NE_pin, 0, 255, 50);
   delay(500);
-  fade(SW_pin, 0, 255, 50);
-  delay(500);
-  fade(SE_pin, 0, 255, 50);
-  delay(500);
-  fade(NE_pin, 0, 255, 50);
+  fadeDown(NE_pin, 255, 0, 50);
+  fadeDown(SE_pin, 255, 0, 50);
+  fadeDown(SW_pin, 255, 0, 50);
+  fadeDown(NW_pin, 255, 0, 50);
 }
 
 void blink(byte pin, unsigned long lightMillis, unsigned long pauseMillis) {
@@ -59,9 +61,24 @@ void blink(byte pin, unsigned long lightMillis, unsigned long pauseMillis) {
   digitalWrite(pin, 0);
 }
 
-void fade(byte pin, byte from, byte to, unsigned long pauseMillis)
+void fadeUp(byte pin, byte from, byte to, unsigned long pauseMillis)
 {
-  for(byte val=from; val<to; val+=5)
+  if (from>=to) return;
+
+  const byte step = 5;
+  for(byte val=from; val<to; val+=step)
+  {
+    analogWrite(pin, val);
+    delay(pauseMillis);  
+  }
+}
+
+void fadeDown(byte pin, byte from, byte to, unsigned long pauseMillis)
+{
+  if (to>=from) return;
+
+  const byte step = 5;
+  for(byte val=from; val>to; val-=step)
   {
     analogWrite(pin, val);
     delay(pauseMillis);  
